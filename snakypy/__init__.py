8c3d6c2af6b5a6bd573e2ca5aa7b4ee048164829
William C. Canin <william.costa.canin@gmail.com>
2020-02-18 04:15:05 -0300

    Add imports in __init__.py

--- snakypy/__init__.py
+++ snakypy/__init__.py
@@ -1,20 +1,20 @@
-# from snakypy.imc import imc
-# from snakypy.pick import pick
-# from snakypy.utilities import (
-#     create_file, create_json, read_json, update_json,
-#     get_shell, is_tool, cmd_popen, loading, notify,
-#     percentage, dot_file_extension, command_real_time,
-#     cleaner, the_credits
-# )
-# from snakypy.decorators import (
-#     use_unix_system
-# )
-# from snakypy.colorful import (
-#     BLACK_COLOR, MAGENTA_COLOR, BLUE_COLOR, GREEN_COLOR,
-#     RED_COLOR, YELLOW_COLOR, CYAN_COLOR, WHITE_COLOR,
-#     WARNING_ALERT, ERROR_ALERT, FINISH_ALERT, QUESTION_ALERT,
-#     NONE_SCOPE_ANSI, printer, entry
-# )
+from snakypy.imc import imc
+from snakypy.pick import pick
+from snakypy.utilities import (
+    create_file, create_json, read_json, update_json,
+    get_shell, is_tool, cmd_popen, loading, notify,
+    percentage, dot_file_extension, command_real_time,
+    cleaner, the_credits
+)
+from snakypy.decorators import (
+    use_unix_system
+)
+from snakypy.colorful import (
+    BLACK_COLOR, MAGENTA_COLOR, BLUE_COLOR, GREEN_COLOR,
+    RED_COLOR, YELLOW_COLOR, CYAN_COLOR, WHITE_COLOR,
+    WARNING_ALERT, ERROR_ALERT, FINISH_ALERT, QUESTION_ALERT,
+    NONE_SCOPE_ANSI, printer, entry
+)
 
 __author__ = {
     'name': 'Snakypy Org',
