cb5a60eec2cdde9d63637226229e548f301917f4
William C. Canin <william.costa.canin@gmail.com>
2020-02-19 01:03:40 -0300

    Enhancing the cmd function in the "ret" and "verbose" parameters

--- snakypy/console/commands.py
+++ snakypy/console/commands.py
@@ -4,16 +4,24 @@ def cmd(command, shell=True, universal_newlines=True, ret=False, verbose=False):
     The function receives a command as an argument and shows
     execution in real time.
     """
-    from subprocess import Popen, PIPE
+    from subprocess import Popen, PIPE, check_call
 
-    process = Popen(command, shell=shell, stdout=PIPE,
-                    universal_newlines=universal_newlines)
     if verbose:
+        process = Popen(command, shell=shell, stdin=PIPE, stdout=PIPE,
+                        universal_newlines=universal_newlines)
         for line in iter(process.stdout.readline, ''):
             print(line.rstrip())
-    if ret:
-        r = process.poll()
-        return r
+        if ret:
+            r = process.poll()
+            return r
+    else:
+        from subprocess import DEVNULL
+        if ret:
+            msg = 'You can only use the "ret=True" option with verbose mode. ' \
+                  'Use verbose=True'
+            raise Exception(msg)
+        Popen(command, shell=shell, stdin=DEVNULL, stdout=DEVNULL,
+              stderr=DEVNULL, universal_newlines=universal_newlines)
 
 
 __all__ = ['cmd']
