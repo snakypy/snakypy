6f255a8655c09ab2659cc53bd808501f9fc7c7cf
William C. Canin <william.costa.canin@gmail.com>
2020-02-19 01:57:07 -0300

    Removing functions and imports from the basic module

--- snakypy/utilities/basic.py
+++ snakypy/utilities/basic.py
@@ -1,9 +1,4 @@
-import time
-import sys
-from snakypy.console.colorful import printer
-from snakypy.ansi import CYAN_COLOR, WARNING_ALERT
 from subprocess import check_output
-from shutil import which
 from snakypy.tools.system import use_unix_system
 
 
@@ -25,62 +20,6 @@ from snakypy.tools.system import use_unix_system
 #     Notify.Notification.new(message, description, dialog).show()
 
 
-def is_tool(name):
-    """Check whether `name` is on PATH and marked as executable.
-    :param name: Enter the name of the program to check if it exists on the machine.
-
-    Arguments:
-        name {[type]} -- [description]
-
-    Returns:
-        [type] -- [description]
-    """
-
-    return which(name) is not None
-
-
-def loading(set_time=0.030, bar=False, header='[Loading]', colorful=False):
-    """[summary]
-
-    Keyword Arguments:
-        set_time {float} -- [description] (default: {0.030})
-        bar {bool} -- [description] (default: {False})
-        header {str} -- [description] (default: {'[Loading]'})
-
-    Returns:
-        [type] -- [description]
-    """
-
-    if colorful:
-        printer(header, style=CYAN_COLOR)
-    else:
-        print(header)
-    try:
-        if bar:
-            for i in range(0, 100):
-                time.sleep(set_time)  # 5 seconds
-                width = (i + 1) / 4
-                bar = f"[{'#' * int(width)} {' ' * (25 - int(width))}]"
-                sys.stdout.write(u"\u001b[1000D" + bar)
-                sys.stdout.flush()
-            # return 'bar'
-        for i in range(0, 100):
-            time.sleep(set_time)
-            sys.stdout.write(u"\u001b[1000D")
-            sys.stdout.flush()
-            # time.sleep(0.1)
-            sys.stdout.write(f'{str(i + 1)}%')
-            sys.stdout.flush()
-        print()
-        # return 'percent'
-    except KeyboardInterrupt:
-        if colorful:
-            printer('\nCanceled by user.', style=WARNING_ALERT)
-        else:
-            print('\nCanceled by user.')
-        return
-
-
 @use_unix_system
 def get_shell():
     """Function to get the currently activated shell.
@@ -95,7 +34,7 @@ def get_shell():
     return lst[2]
 
 
-def dot_file_extension(filename):
+def file_extension(filename):
     """[summary]
 
     Arguments:
@@ -112,4 +51,4 @@ def dot_file_extension(filename):
     return m.group(0)
 
 
-__all__ = ['get_shell', 'is_tool', 'loading', 'dot_file_extension']
+__all__ = ['get_shell', 'file_extension']
