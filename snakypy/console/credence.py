0d9cd2abe13a758cb6d22d64db2222b307cf6fca
William C. Canin <william.costa.canin@gmail.com>
2020-02-19 00:09:23 -0300

    Credence function acquired color customization on output. Ansicolor parameter.

--- snakypy/console/credence.py
+++ snakypy/console/credence.py
@@ -1,8 +1,4 @@
-from snakypy.tools.system import use_unix_system
-
-
-@use_unix_system
-def credence(app_name, app_version, app_url, data: dict):
+def credence(app_name, app_version, app_url, data: dict, ansicolor=''):
     """
     Print project development credits. Example:
     data = {
@@ -16,7 +12,7 @@ def credence(app_name, app_version, app_url, data: dict):
     """
 
     from datetime import date
-    from snakypy.ansi import CYAN_COLOR, NONE_SCOPE_ANSI
+    from snakypy.console import printer
 
     try:
         if type(data) is not dict:
@@ -24,18 +20,26 @@ def credence(app_name, app_version, app_url, data: dict):
                 'must take a dictionary as an argument.'
             raise Exception(msg)
 
-        print(CYAN_COLOR, f'{57 * "-"}'.center(75))
-        print(f'{app_name} - Version {app_version}'.center(70))
-        print(f'{57 * "-"}\n'.center(75))
+        # print(CYAN_COLOR, f'{57 * "-"}'.center(75))
+        printer(f'{57 * "-"}'.center(75), style=ansicolor)
+        # print(f'{app_name} - Version {app_version}'.center(70))
+        printer(f'{app_name} - Version {app_version}'.center(70), style=ansicolor)
+        # print(f'{57 * "-"}\n'.center(75))
+        printer(f'{57 * "-"}\n'.center(75), style=ansicolor)
+        printer(f'Credence:\n'.center(70), style=ansicolor)
         for item in data['credence']:
             for key, value in item.items():
-                print(f'{key.title().replace("_", " ")}: {value}'.center(70))
-
+                # print(f'{key.title().replace("_", " ")}: {value}'.center(70))
+                printer(f'{key.title().replace("_", " ")}: {value}'.center(70), style=ansicolor)
         print()
-        print(f'{57 * "-"}'.center(75))
-        print(f'{app_name} © {date.today().year} - All Right Reserved.'.center(70))
-        print(f'Home: {app_url}'.center(70))
-        print(f'{57 * "-"}'.center(75), NONE_SCOPE_ANSI)
+        # print(f'{57 * "-"}'.center(75))
+        printer(f'{57 * "-"}'.center(75), style=ansicolor)
+        # print(f'{app_name} © {date.today().year} - All Right Reserved.'.center(70))
+        printer(f'{app_name} © {date.today().year} - All Right Reserved.'.center(70), style=ansicolor)
+        # print(f'Home: {app_url}'.center(70))
+        printer(f'Home: {app_url}'.center(70), style=ansicolor)
+        # print(f'{57 * "-"}'.center(75), NONE_SCOPE_ANSI)
+        printer(f'{57 * "-"}'.center(75), style=ansicolor)
     except KeyError:
         msg = "The 'credence' key was not found." \
               "Enter a dictionary containing a 'credits' key."
