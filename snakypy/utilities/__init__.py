c3882a5609e93d041273007b36445616552b71db
William C. Canin <william.costa.canin@gmail.com>
2020-02-19 01:56:06 -0300

    Updating the dander init from the utilities subpackage. Adding and removing imports.

--- snakypy/utilities/__init__.py
+++ snakypy/utilities/__init__.py
@@ -1,5 +1,2 @@
-from .percentage import percentage
-from .ruthless import cleaner
-from .utilities import get_shell, dot_file_extension
-from .utilities import is_tool
-from .bmi import bmi
+from .stark import cleaner
+from .basic import get_shell, file_extension
