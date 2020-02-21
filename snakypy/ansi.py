23c0620e4dd95abaec1526a517e03a07f8b0c109
William C. Canin <william.costa.canin@gmail.com>
2020-02-21 02:06:14 -0300

    Adding the SGR class, the "return_ansi" function to check if it is Windows OS and docstring.

--- snakypy/ansi.py
+++ snakypy/ansi.py
@@ -1,37 +1,74 @@
-NONE = '\x1b[0m'
-""""""
+"""Module to store Ansi configurations.
+In certain functions of the snakypy package, functions with named
+parameters "fg" and "bg" are found, in which you can receive a
+certain foreground or background color in Ansi.
+"""
+import sys
+
+
+def return_ansi(value):
+    if sys.platform.startswith('win'):
+        return ''
+    return value
+
+
+NONE = return_ansi('\x1b[0m')
+"""Variable to reset Ansi color settings."""
 
 
 class FG:
-    BLACK = '\x1b[30m'
-    MAGENTA = '\x1b[95m'
-    BLUE = '\x1b[94m'
-    GREEN = '\x1b[92m'
-    RED = '\x1b[91m'
-    YELLOW = '\x1b[93m'
-    CYAN = '\x1b[96m'
-    WHITE = '\x1b[97m'
+    """
+    Class FG (foreground) that receives colors and ANSI settings in each of
+    the global variables below to be applied in texts.
+    """
 
-    WARNING = f'{YELLOW}⚠ '
-    ERROR = f'{RED}✖ '
-    FINISH = f'{GREEN}✔ '
-    QUESTION = f'{CYAN}➜ '
+    BLACK = return_ansi('\x1b[30m')
+    MAGENTA = return_ansi('\x1b[95m')
+    BLUE = return_ansi('\x1b[94m')
+    GREEN = return_ansi('\x1b[92m')
+    RED = return_ansi('\x1b[91m')
+    YELLOW = return_ansi('\x1b[93m')
+    CYAN = return_ansi('\x1b[96m')
+    WHITE = return_ansi('\x1b[97m')
+
+    WARNING = return_ansi(f'{YELLOW}⚠ ')
+    ERROR = return_ansi(f'{RED}✖ ')
+    FINISH = return_ansi(f'{GREEN}✔ ')
+    QUESTION = return_ansi(f'{CYAN}➜ ')
 
 
 class BG:
-    BLACK = '\x1b[40m'
-    MAGENTA = '\x1b[105m'
-    BLUE = '\x1b[104m'
-    GREEN = '\x1b[102m'
-    RED = '\x1b[101m'
-    YELLOW = '\x1b[103m'
-    CYAN = '\x1b[106m'
-    WHITE = '\x1b[107m'
+    """
+    Class BG (background) that receives colors and ANSI settings in each
+    of the global variables below to be applied in texts.
+    """
 
-    WARNING = f'{YELLOW}⚠ '
-    ERROR = f'{RED}✖ '
-    FINISH = f'{GREEN}✔ '
-    QUESTION = f'{CYAN}➜ '
+    BLACK = return_ansi('\x1b[40m')
+    MAGENTA = return_ansi('\x1b[105m')
+    BLUE = return_ansi('\x1b[104m')
+    GREEN = return_ansi('\x1b[102m')
+    RED = return_ansi('\x1b[101m')
+    YELLOW = return_ansi('\x1b[103m')
+    CYAN = return_ansi('\x1b[106m')
+    WHITE = return_ansi('\x1b[107m')
+
+    WARNING = return_ansi(f'{YELLOW}⚠ ')
+    ERROR = return_ansi(f'{RED}✖ ')
+    FINISH = return_ansi(f'{GREEN}✔ ')
+    QUESTION = return_ansi(f'{CYAN}➜ ')
 
 
-__all__ = ['NONE', 'FG', 'BG']
+class SGR:
+    """
+
+    """
+
+    BOLD = return_ansi('\x1b[1m')
+    ITALIC = return_ansi('\x1b[3m')
+    UNDERLINE = return_ansi('\x1b[4m')
+    SLOW_BLINK = return_ansi('\x1b[5m')
+    RAPID_BLINK = return_ansi('\x1b[6m')
+    REVERSE_COLOR = return_ansi('\x1b[7m')
+
+
+__all__ = ['NONE', 'FG', 'BG', 'SGR']
