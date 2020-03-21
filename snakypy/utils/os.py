f45a15f8bb3577c8406b3b594e0ce5ad336e53a1
William C. Canin <william.costa.canin@gmail.com>
2020-03-21 13:10:53 -0300

    :hammer: Assigning try / except in the function rmdir_blank in the utils.os module.

--- snakypy/utils/os.py
+++ snakypy/utils/os.py
@@ -1,6 +1,4 @@
 import os
-import contextlib
-from os.path import join
 
 
 def rmdir_blank(path):
@@ -9,5 +7,9 @@ def rmdir_blank(path):
     for r, d, f in os.walk(path, topdown=False):
         for folder in d:
             if len(os.listdir(join(r, folder))) == 0:
-                with contextlib.suppress(Exception):
-                    os.rmdir(join(r, folder))
+                try:
+                    os.rmdir(os.path.join(r, folder))
+                except PermissionError:
+                    raise PermissionError("No permission to remove empty folders")
+                except Exception:
+                    raise Exception("It was not possible to clean empty folders.")
